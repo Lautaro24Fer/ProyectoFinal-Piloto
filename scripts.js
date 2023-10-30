622a2448d6e7ffc66ce63d0c1327056ae00aaff4
Lautaro <LautaroFer456@gmail.com>
2023-10-29 21:06:30 -0300

    TratandoDeArreglarJS

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const asideEl = document.querySelector("#asideAccountInfo")
+    const abrirAsideEl = document.querySelector("#btnAside")
+    const cerrarAsideEl = document.querySelector("#btnCerrarAside")
+
+    abrirAsideEl.addEventListener("click", ()=>{
+      if (window.innerWidth <=700){
+        asideEl.style.left = "0";
+        asideEl.style.boxShadow = "122px 0px 0px 0px rgba(0,0,0,0.70)";
+      }
+    })
+
+    cerrarAsideEl.addEventListener("click", ()=>{
+    if (window.innerWidth <=700){
+      asideEl.style.left = "-100%";
+      asideEl.style.boxShadow = "0px 0px 0px 0px rgba(0,0,0,0.70)";
+    }
+    })
